1abf354e88557f8f029cefb815700c9bb1e8179a
umbra070 <umbra070@yandex.ru>
2026-01-08 19:12:54 +0300

    Les7v1

--- src/Main.java
+++ src/Main.java
@@ -14,31 +14,66 @@ void main() {
     //Ex2
     System.out.println("\n\rEx2\n\r");
     System.out.println("\n\rInt array:\n\r");
-    for (int i : intArr) {
-        System.out.println(i);
+    String intArrStr = "";
+    for(int i = 0; i < intArr.length; i++){
+        if(i != (intArr.length - 1)){
+            intArrStr = intArrStr.concat(String.format("%d, ", intArr[i]));
+        }else{
+            intArrStr = intArrStr.concat("" + intArr[i]);
+        }
     }
+    System.out.println(intArrStr);
     System.out.println("\n\rFloat array:\n\r");
-    for (float f : floatArr) {
-        System.out.println(f);
+    String floatArrStr = "";
+    for(int i = 0; i < floatArr.length; i++){
+        if(i != (floatArr.length - 1)){
+            floatArrStr = floatArrStr.concat(String.format("%f, ", floatArr[i]));
+        }else{
+            floatArrStr = floatArrStr.concat("" + floatArr[i]);
+        }
     }
+    System.out.println(floatArrStr);
     System.out.println("\n\rString array:\n\r");
-    for (String s : strArr) {
-        System.out.println(s);
+    String strArrStr = "";
+    for(int i = 0; i < strArr.length; i++){
+        if(i != (strArr.length - 1)){
+            strArrStr = strArrStr.concat(String.format("%s, ", strArr[i]));
+        }else{
+            strArrStr = strArrStr.concat(strArr[i]);
+        }
     }
+    System.out.println(strArrStr);
     //Ex3
-    System.out.println("\n\rEx3\n\r");
     System.out.println("\n\rInt array:\n\r");
-    for (int i = intArr.length - 1; i >= 0; i--) {
-        System.out.println(intArr[i]);
+    String intArrStrInvert = "";
+    for(int i = intArr.length-1; i >= 0; i--){
+        if(i != 0){
+            intArrStrInvert = intArrStrInvert.concat(String.format("%d, ", intArr[i]));
+        }else{
+            intArrStrInvert = intArrStrInvert.concat("" + intArr[i]);
+        }
     }
+    System.out.println(intArrStrInvert);
     System.out.println("\n\rFloat array:\n\r");
-    for (int i = floatArr.length - 1; i >= 0; i--) {
-        System.out.println(floatArr[i]);
+    String floatArrStrInvert = "";
+    for(int i = floatArr.length-1; i >= 0; i--){
+        if(i != 0){
+            floatArrStrInvert = floatArrStrInvert.concat(String.format("%f, ", floatArr[i]));
+        }else{
+            floatArrStrInvert = floatArrStrInvert.concat("" + floatArr[i]);
+        }
     }
+    System.out.println(floatArrStrInvert);
     System.out.println("\n\rString array:\n\r");
-    for (int i = strArr.length - 1; i >= 0; i--) {
-        System.out.println(strArr[i]);
+    String strArrStrInvert = "";
+    for(int i = strArr.length-1; i >= 0; i--){
+        if(i != 0){
+            strArrStrInvert = strArrStrInvert.concat(String.format("%s, ", strArr[i]));
+        }else{
+            strArrStrInvert = strArrStrInvert.concat(strArr[i]);
+        }
     }
+    System.out.println(strArrStrInvert);
     //Ex4
     System.out.println("\n\rEx4\n\r");
     for (int ind = 0; ind < intArr.length; ind++) {
